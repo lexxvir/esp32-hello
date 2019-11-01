e38900abf689f2d823c4d11212069226ad112c2b
Matthew Caron <matt@mattcaron.net>
2019-10-31 20:39:19 -0400

    Added safety message so it's clear which code is running.
    Brought to you by the fact that I wasted an hour flashing the wrong code.

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,1 +1,7 @@
-void app_main() {}
+#include "esp_log.h"
+
+void app_main() {
+    ESP_LOGI("C Main",
+             "You should not get here. Eiher the linker is broken,\n"
+             "or you flashed the wrong code..");
+}
